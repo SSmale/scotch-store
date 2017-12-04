fd764902516c63ebd2984b07299cb66d72da3629
SimonSmale <simon@smalemail.net>
2017-12-04 19:32:43 +0000

    Inital page templates and router setup, ES diabled as it was conflicting with Beautify

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,27 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import HelloWorld from '@/components/HelloWorld';
+
+import Home from '@/pages/Home';
+import Admin from '@/pages/Admin';
+import Cart from '@/pages/Cart';
 
 Vue.use(Router);
 
 export default new Router({
-  routes: [
-    {
+  routes: [{
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld,
+      name: 'Home',
+      component: Home,
     },
+    {
+      path: '/admin',
+      name: 'Admin',
+      component: Admin
+    },
+    {
+      path: '/cart',
+      name: 'Cart',
+      component: Cart
+    }
   ],
 });
